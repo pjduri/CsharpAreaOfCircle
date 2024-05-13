ce25f4527be942d15aa97ec90fca46815c801170
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-12 22:53:11 -0500

    studio done no bonus

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -8,6 +8,14 @@ double area = pi * Math.Pow(radius, 2);
 double circumference = 2 * radius * pi;
 double diameter = 2 * radius;
 
-Console.WriteLine($"A circle with a radius of {radius}cm has an area of {area} square cm,");
-Console.WriteLine($"a circumference of {circumference} cm,");
-Console.WriteLine($"and a diameter of {diameter}cm.");
+Console.WriteLine($"A circle with a radius of {radius}mi has an area of {area} square mi,");
+Console.WriteLine($"a circumference of {circumference} mi,");
+Console.WriteLine($"and a diameter of {diameter}mi.");
+
+Console.WriteLine("\nNow let's figure out how much gas you need to get around a circle like that.");
+Console.WriteLine("What is your car's mpg?");
+string? mpgStr = Console.ReadLine();
+double mpg = double.Parse(mpgStr);
+
+double gallonsToCircumnavigate = circumference / mpg;
+Console.WriteLine($"Your car will need {gallonsToCircumnavigate} gallons of gas to get around the circle.");
