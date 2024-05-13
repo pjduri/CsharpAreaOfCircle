a4753884b3a06d45326eb409d655d79d3d0f9ae0
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-12 22:39:52 -0500

    part 2 add circumference and diameter

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -5,5 +5,9 @@ double radius = double.Parse(input);
 double pi = Math.PI;
 
 double area = pi * Math.Pow(radius, 2);
+double circumference = 2 * radius * pi;
+double diameter = 2 * radius;
 
 Console.WriteLine($"A circle with a radius of {radius}cm has an area of {area} square cm,");
+Console.WriteLine($"a circumference of {circumference} cm,");
+Console.WriteLine($"and a diameter of {diameter}cm.");
