a0d73feb3d1fc6236a0ca574f5f989f51f58cdaf
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-12 22:39:12 -0500

    part one, area of circle

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+Console.WriteLine("Hello!  Please enter the radius of your circle.");
+string input = Console.ReadLine();
+double radius = double.Parse(input);
+double pi = Math.PI;
+
+double area = pi * Math.Pow(radius, 2);
+
+Console.WriteLine($"A circle with a radius of {radius}cm has an area of {area} square cm,");
