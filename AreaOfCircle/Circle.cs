5f12ad3a8ff657ab1fa901f2ee2aea7ce207c91e
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-12 23:48:27 -0500

    bonus mission 1

---
+++ AreaOfCircle/Circle.cs
@@ -0,0 +1,33 @@
+﻿namespace AreaOfCircle;
+
+public class Circle
+{
+    public double Radius { get; set; }
+    private double area;
+    public double Area
+    {
+        get { return area; }
+        set { area = Math.PI * Math.Pow(Radius, 2); }
+    }
+    private double circumference;
+    public double Circumference 
+    { 
+        get { return circumference; }
+        set { circumference = Math.PI * Radius* 2; }
+    }
+
+    private double diameter;
+    public double Diameter
+    {
+        get { return diameter; }
+        set { diameter = Radius * 2; }
+    }
+
+    public Circle(double radius) {
+        Radius = radius;
+        area = Math.PI * Math.Pow(Radius, 2);
+        circumference = 2 * Math.PI * Radius;
+        diameter = 2 * Radius;
+    }
+
+}
--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -1,21 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AreaOfCircle;
+
 Console.WriteLine("Hello!  Please enter the radius of your circle.");
 string input = Console.ReadLine();
 double radius = double.Parse(input);
-double pi = Math.PI;
+Circle circle = new(radius);
 
-double area = pi * Math.Pow(radius, 2);
-double circumference = 2 * radius * pi;
-double diameter = 2 * radius;
 
-Console.WriteLine($"A circle with a radius of {radius}mi has an area of {area} square mi,");
-Console.WriteLine($"a circumference of {circumference} mi,");
-Console.WriteLine($"and a diameter of {diameter}mi.");
+Console.WriteLine($"A circle with a radius of {radius}mi has an area of {circle.Area} square mi,");
+Console.WriteLine($"a circumference of {circle.Circumference} mi,");
+Console.WriteLine($"and a diameter of {circle.Diameter}mi.");
 
 Console.WriteLine("\nNow let's figure out how much gas you need to get around a circle like that.");
 Console.WriteLine("What is your car's mpg?");
 string? mpgStr = Console.ReadLine();
 double mpg = double.Parse(mpgStr);
 
-double gallonsToCircumnavigate = circumference / mpg;
+double gallonsToCircumnavigate = circle.Circumference / mpg;
 Console.WriteLine($"Your car will need {gallonsToCircumnavigate} gallons of gas to get around the circle.");
